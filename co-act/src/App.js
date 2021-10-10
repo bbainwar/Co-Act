8c26d2de0caf7a2c0be623612d0ffd2ee371f3a1
roatt-tilma <roshanbhattmnr@gmail.com>
2021-10-10 20:43:46 +0545

    Project setup

---
+++ co-act/src/App.js
@@ -0,0 +1,14 @@
+import Titlebox from './Titlebox';
+
+function App() {
+  return (
+    <div className="App">
+      <Titlebox />
+      <div className="content">
+        <h1>App Component</h1>
+      </div>      
+    </div>
+  );
+}
+
+export default App;
---
+++ co-act/src/Titlebox.js
@@ -0,0 +1,9 @@
+const Titlebox = () => {
+    return ( 
+        <div className="titlebox">
+            <h1>Co-ACT</h1>
+        </div>
+    );
+}
+ 
+export default Titlebox;
