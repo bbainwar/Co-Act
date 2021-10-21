10833176406e2f0cd60ece42db2b793a6a5c7497
roatt-tilma <roshanbhattmnr@gmail.com>
2021-10-21 21:53:26 +0545

    added backend

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, {    
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully!');
+});
+
+const usersRouter = require('./routes/users');
+
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+    console.log(`The server is running on port: ${port}`);
+});
+
+
+
+
+
