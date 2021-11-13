87727a77c7438bb0648b232ab4de1da3ede9cd80
Subham-RKB <subhamchapagain45@gmail.com>
2021-11-13 16:04:06 +0545

    Cospace.js

---
+++ backend/routes/cospace.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+let cospace = require("./models/cospace.model");
+
+router.route("/").get((req, req) => {
+  cospace
+    .find()
+    .then((cospace) => res.json(cospace))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+router.route("/add").post((req, res) => {
+  const cospacename = req.body.cospacename;
+  const description = req.body.description;
+  const coactors = req.body.coactors;
+  const date = Date.parse(req.body.date);
+  const newCospace = new cospace({
+    cospacename,
+    description,
+    coactors,
+    date,
+  });
+  newCospace
+    .save()
+    .then(() => res.json("Cospace Created!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,35 +13,41 @@ app.use(express.urlencoded());
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log('MongoDB database connection established successfully!');
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully!");
 });
 
-const usersRouter = require('./routes/users');
+const usersRouter = require("./routes/users");
 
-const adminRouter = require('./routes/admin');
+const adminRouter = require("./routes/admin");
 
-const contentsRouter = require('./routes/contents');
+const contentsRouter = require("./routes/contents");
 
-const settingsRouter = require('./routes/settings');
+const settingsRouter = require("./routes/settings");
 
-app.use('/users', usersRouter);
+const cospaceRouter = require("./routes/cospace");
 
-app.use('/admin', adminRouter);
+app.use("/users", usersRouter);
 
-app.use('/contents', contentsRouter);
+app.use("/admin", adminRouter);
 
-app.use('/settings', settingsRouter);
+app.use("/contents", contentsRouter);
+
+app.use("/settings", settingsRouter);
+
+app.use("/cospce", cospaceRouter);
 
 app.use((req, res) => {
-    res.send("Sorry! The page you are looking for is currently unavailable. Kindly contact bbainwar@gmail.com if you have any queries!");
-})
+  res.send(
+    "Sorry! The page you are looking for is currently unavailable. Kindly contact bbainwar@gmail.com if you have any queries!"
+  );
+});
 
 app.listen(port, () => {
-    console.log(`The server is running on port: ${port}`);
+  console.log(`The server is running on port: ${port}`);
 });
